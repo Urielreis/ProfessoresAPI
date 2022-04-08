6d5fe1102cb62d4a1a6809023da7d7d0677dad4b
Urielreis <urielreis1992@yahoo.com.br>
2022-04-08 09:02:45 -0500

    Finalizado

---
+++ ProfessoresAPI/ProfessorModel.swift
@@ -0,0 +1,15 @@
+//
+//  ProfessorModel.swift
+//  ProfessoresAPI
+//
+//  Created by user213596 on 4/4/22.
+//
+
+//import Foundation
+
+class ProfessorModel : Codable {
+    
+    var id : Int?
+    var nome : String?
+    var email : String?
+}
